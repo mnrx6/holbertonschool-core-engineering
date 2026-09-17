3f2518cb583f6d5889e06d74eefa76b32eddebe5
mnrx6 <mnrx78@gmail.com>
2026-09-17 11:42:20 +0200

    add task 1

---
+++ python_fundamentals/functions_modules/uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def uppercase(str):
+    for letter in str:
+        if ord(letter) >= ord("a") and ord(letter) <= ord("z"):
+            letter = chr(ord(letter) - 32)
+            print("{}".format(letter), end="")
+        else:
+            print("{}".format(letter), end="")
+    print()
