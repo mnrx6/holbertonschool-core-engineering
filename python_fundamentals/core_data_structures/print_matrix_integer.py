8aa2b2bbf68a921ac2be74b8d05f8745cc553636
mnrx6 <mnrx78@gmail.com>
2026-09-21 09:26:38 +0200

    add task 3

---
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for num in range(len(row)):
+            if num != len(row)-1:
+                print("{:d}".format(row[num]), end=" ")
+            else:
+                print("{:d}".format(row[num]))
