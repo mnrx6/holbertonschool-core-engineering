efd0c12adbd8b4d9bae20928cbc8e8d4081fe39e
mnrx6 <mnrx78@gmail.com>
2026-09-21 09:33:57 +0200

    fix task 3

--- python_fundamentals/core_data_structures/print_matrix_integer.py
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -3,7 +3,8 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for num in range(len(row)):
-            if num != len(row)-1:
+            if num != len(row) - 1:
                 print("{:d}".format(row[num]), end=" ")
             else:
-                print("{:d}".format(row[num]))
+                print("{:d}".format(row[num]), end="")
+        print()
