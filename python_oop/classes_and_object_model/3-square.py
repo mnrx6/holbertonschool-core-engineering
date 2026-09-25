fdf6f066bf59a14ed5230c8659b3eec593127630
mnrx6 <mnrx78@gmail.com>
2026-09-25 02:53:02 +0200

    add task 3 and 4-5-6 files

---
+++ python_oop/classes_and_object_model/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Defines a Square class."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """Initialize a square."""
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
