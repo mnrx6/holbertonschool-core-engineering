d951448fcce0db45eadc91c0e3466ef736488833
mnrx6 <mnrx78@gmail.com>
2026-09-25 14:03:41 +0200

    add task 4

--- python_oop/classes_and_object_model/4-square.py
+++ python_oop/classes_and_object_model/4-square.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python3
+
+class Square :
+    """Represents a square."""
+    def __init__(self, size=0):
+        """Initialize a square."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Return the size."""
+        return self.__size
+    @size.setter
+        def size(self, value):
+            """Set the size."""
+            if type(value) is not int:
+                return TypeError("size must be an integer")
+            if value < 0:
+                return ValueError("size must be >= 0")
+
+            self.__size = value
+
+     def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size   
